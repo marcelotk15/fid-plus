79c50300f7ecb30c6402b599c3ef97a6dd44d112
Marcelo 'teka' Oliveira <marcelotk_15@hotmail.com>
2026-07-23 12:20:37 -0300

    refactor(quiz): migrate route change payload and remove unused quiz modules

--- src/modules/shared/register-fetch-intercept-rules.ts
+++ src/modules/shared/register-fetch-intercept-rules.ts
@@ -1,4 +1,3 @@
-import { QuizFetchInterceptRuleRunner } from '~/modules/quiz/quiz-fetch-bridge'
 import { FetchInterceptRuleRegistry } from '~/modules/shared/fetch-intercept-rule-runner'
 
-export const fetchInterceptRuleRegistry = new FetchInterceptRuleRegistry([new QuizFetchInterceptRuleRunner()])
+export const fetchInterceptRuleRegistry = new FetchInterceptRuleRegistry([])
